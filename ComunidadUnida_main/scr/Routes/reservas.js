6f03dac2081260f8d72a26bc5267879e8f532695
mverschuerenp <martin.verschueren@gmail.com>
2022-06-13 19:00:05 -0400

    Agregar hipervinculo para Admin

--- ComunidadUnida_main/scr/Routes/reservas.js
+++ ComunidadUnida_main/scr/Routes/reservas.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Reservas = require('../controladores/Contr_reservas');
 
-router.get('/admin', async (req,res) => {
+router.get('/', async (req,res) => {
     let reservas = await new Reservas().getReservas();
     return res.render('pagina',{reservas});
 });
