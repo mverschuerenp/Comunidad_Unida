2c91c0926f048a8898493c0fbe4149df6444a952
criveliz <ignacio.veliz1234@gmail.com>
2022-06-14 00:41:25 -0400

    okidoki

--- scr/Routes/reservas.js
+++ scr/Routes/reservas.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Reservas = require('../controladores/Contr_reservas');
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     let reservas = await new Reservas().getReservas();
-    return res.render('pagina',{reservas});
+    return res.render('pagina', { reservas });
 });
 
+router.get('/admin', async (req, res) => {
+    let reservas = await new Reservas().getReservas();
+    return res.render('pagina_admin', { reservas });
+});
 
 
 module.exports = router;
