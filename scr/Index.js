241e6e4ff10c39c0a837b3c5588e95d1ea30801b
criveliz <ignacio.veliz1234@gmail.com>
2022-07-04 17:58:22 -0400

    reordenar carpetas

--- scr/Index.js
+++ scr/Index.js
@@ -4,19 +4,19 @@ var parser = require('body-parser');
 const app = express();
 const reservasRoutes = require('./Routes/reservas.js');
 
-app.set("view engine","ejs");
-app.set("views",__dirname+"\134views");
+app.set("view engine", "ejs");
+app.set("views", __dirname + "\134views\134pages");
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(reservasRoutes);
 
 app.use(cors());
 
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
 const PORT = process.env.PORT || 4000;
